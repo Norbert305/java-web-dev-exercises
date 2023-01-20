f02d28e935f70bfc59a4df1f1bc61b40f07280d5
Norbert305 <sealsnorbert@yahoo.com>
2023-01-20 15:53:55 -0500

    technologist exercises Name and Rectangle area

---
+++ src/exercises/Rectangle.java
@@ -0,0 +1,19 @@
+package exercises;
+import java.util.Scanner;
+public class Rectangle {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the length of your rectangle please?:");
+        double l = input.nextDouble();
+        System.out.println("What is the width of your rectangle please?:");
+        double w = input.nextDouble();
+        double area = l*w;
+        System.out.println("The area of your rectangle is " + area);
+
+
+
+
+    }
+}
