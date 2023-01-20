d8e8ad4adc57f193d6781cff86ac55502fbea853
Norbert305 <sealsnorbert@yahoo.com>
2023-01-20 17:08:28 -0500

    technologist exercises Gas Miles

---
+++ src/exercises/CarDistance.java
@@ -0,0 +1,18 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class CarDistance {
+    //25 miles = 1 gallon: Please use this logic to receive an appropriate answer
+    //Goal is to calculate how many miles per gallon.
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many miles have you driven today total?:");
+        double m = input.nextDouble();
+        System.out.println("How many gallons of gas did you put in your tank?:");
+        double g = input.nextDouble();
+        double milesPerGallon = m/g;
+        System.out.println("The total miles per gallon is " + milesPerGallon);
+    }
+
+}
