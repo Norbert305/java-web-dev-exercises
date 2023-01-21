60c128353932f411b9d2eb008f284e47108141f1
Norbert305 <sealsnorbert@yahoo.com>
2023-01-20 19:40:46 -0500

    technologist exercises word search Alice

---
+++ src/exercises/WordSearch.java
@@ -0,0 +1,35 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class WordSearch {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String decision;
+        while(true){
+
+            System.out.println("please enter the main Character: Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do");
+            String name = input.nextLine();
+
+            System.out.println("you entered the name " + name );
+
+            System.out.println("is this name correct : Lets see!! What is the name again??");
+            decision = input.nextLine();
+
+            switch(decision){
+                case "Alice":
+                    System.out.println("You guessed correctly!!! the name was "+name);
+                    break;
+                case "alice":
+                    System.out.println("You guessed correctly!! the name was "+name);
+                    break;
+                default :
+                    System.out.println("You guessed wrong!!! please enter again ");
+
+            }
+
+        }
+    }
+}
