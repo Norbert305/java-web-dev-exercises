d8a48ca78fcdf6b0ecd28c0b1ee125532e3eb9df
Norbert305 <sealsnorbert@yahoo.com>
2023-01-24 10:20:25 -0500

    Array List source material

--- src/ArrayPractice/Main.java
+++ src/ArrayPractice/Main.java
@@ -2,7 +2,8 @@ package ArrayPractice;
 import java.util.ArrayList;
 import java.util.Collections;
 public class Main {
-
+    //Having fun with array list
+    //source: https://www.w3schools.com/java/java_arraylist.asp
     public static void main(String[] args) {
         ArrayList<String> cars = new ArrayList<>();
         //method add appends the values to make an array list
