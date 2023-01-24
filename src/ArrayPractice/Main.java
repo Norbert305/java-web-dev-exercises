34983b75c2483d27c7cdbce25433dd0da68ed716
Norbert305 <sealsnorbert@yahoo.com>
2023-01-24 15:05:10 -0500

    Java Tests

--- src/ArrayPractice/Main.java
+++ src/ArrayPractice/Main.java
@@ -22,8 +22,8 @@ public class Main {
         System.out.println(cars.size());
 
         //How to use the for loop for the array list
-        for (int i = 0; i < cars.size(); i++) {
-            System.out.println(cars.get(i));
+        for (String car : cars) {
+            System.out.println(car);
         }
         System.out.println("---------------");
         //How to use the for-each loop for the array list
